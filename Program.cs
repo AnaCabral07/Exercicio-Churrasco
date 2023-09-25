7f7b3597c1393078de3a19039bb8e97a252e3df5
Ana Clara <4n4.informatica@gmail.com>
2023-09-24 22:05:58 -0300

    Finaliza Programa

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+int adultosComAlcool, adultosSemAlcool, criancas, adultos, pessoas;
+double carne, acompanhamentos, cerveja, refrigerante, agua;
+
+Console.WriteLine("--- Churrasco ---\n");
+
+Console.Write("Adultos (alcoólatras).......: ");
+adultosComAlcool = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Adultos (não alcoólatras)...: ");
+adultosSemAlcool = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Crianças....................: ");
+criancas = Convert.ToInt32(Console.ReadLine());
+
+adultos = adultosComAlcool + adultosSemAlcool;
+pessoas = adultos + criancas;
+
+carne = 0.4 * adultos + 0.2 * criancas;
+acompanhamentos = 0.2 * pessoas;
+cerveja = 2 * adultosComAlcool;
+refrigerante = 0.5 * (adultosSemAlcool + criancas);
+agua = 0.4 * pessoas;
+
+Console.WriteLine();
+
+Console.WriteLine($"Carne.............: {carne:N1}kg");
+Console.WriteLine($"Acompanhamentos...: {acompanhamentos:N1}kg");
+Console.WriteLine($"Cerveja...........: {cerveja:N1}l");
+Console.WriteLine($"Refrigerante......: {refrigerante:N1}l");
+Console.WriteLine($"Água..............: {agua:N1}l");
